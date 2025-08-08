763a7c3abc461a862396e517d50487010f87b55e
KevinSmhevin <KevinSmhevin@users.noreply.github.com>
2025-08-08 15:57:59 -0700

    clean-up-code

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,6 @@ export default function Header() {
     return (
         <header>
             <img src={chefIcon} alt="Chef Quackly Icon" />
-            {/* <h1>Chef Quackly</h1> */}
         </header>
     )
 }
